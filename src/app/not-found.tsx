7c22237d34ad6be98cd4a8e799cb54aaa6a47cf3
FarooqueMalik71 <farooquemalikbusniess@gmail.com>
2025-02-06 06:43:18 +0500

    msg update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,14 @@
 
 export default function notfound() {
   return (
-    <div>Eror 404</div>
+    <div className="min-h-screen bg-gray-50">
+    <div className="flex flex-col justify-center items-center text-center py-20">
+      <div className="bg-white p-10 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-green-500 mb-4">Order Success!</h1>
+        <h3 className="text-xl text-gray-700">Thank you for your purchase.</h3>
+        <p className="text-gray-500 mt-2">Your order has been placed successfully and is being processed.</p>
+      </div>
+    </div>
+    </div>
   )
 }
